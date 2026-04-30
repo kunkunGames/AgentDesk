32d3eed8c1adaab1f0b46d57a3898b3765670219
이택훈 <123369lee@naver.com>
2026-05-01 00:44:10 +0900

    Fix: restore deprecated !model handler to intercept command

--- src/services/discord/commands/text_commands.rs
+++ src/services/discord/commands/text_commands.rs
@@ -681,6 +681,16 @@ Any other message is sent to {p}.
         }
 
 
+        "!model" => {
+            let _ = msg
+                .reply(
+                    &ctx.http,
+                    "Model picker text commands are deprecated. Use `/model`.",
+                )
+                .await;
+            return Ok(true);
+        }
+
         "!allowed" => {
             let ts = chrono::Local::now().format("%H:%M:%S");
             tracing::info!("  [{ts}] ◀ [{}] !allowed {}", msg.author.name, arg1);
