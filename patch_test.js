8eb8ad5cb4e88bbd16a44ea4e4b6c9bd3830db9a
쿤쿤 <kunkun@kunkun-ui-Macmini.local>
2026-05-06 15:53:53 +0900

    chore: remove patch artifact

--- patch_test.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< SEARCH
-  const loadAlertContext = module._loadCardAlertContext;
-=======
-  const loadAlertContext = module._loadCardAlertContext || module.policy._loadCardAlertContext || module.policy;
->>>>>>> REPLACE
