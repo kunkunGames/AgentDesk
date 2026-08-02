7fd54b6c69468bd3cd13892c66a12005c9a99be9
이택훈 <123369lee@naver.com>
2026-08-03 00:04:43 +0900

    Upgrade dashboard security baseline

--- dashboard/src/app/HomeWidgetSpecs.tsx
+++ dashboard/src/app/HomeWidgetSpecs.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { ChevronRight, Flame, Gauge, Sparkles, Zap } from "lucide-react";
 
 import AgentAvatar from "../components/AgentAvatar";
